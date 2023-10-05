1f0a90476a0212e60a61dd065f1a9ed7d7a3c593
adamzabek <adam.zabek@innobiotix.com>
2023-10-05 21:21:47 +0200

    update

--- web_app_hard_hat_detector.py
+++ web_app_hard_hat_detector.py
@@ -38,9 +38,11 @@ st.write(custom_css, unsafe_allow_html=True)
 
 st.title("Hard Hat Detector")
 
+st.write(custom_css, unsafe_allow_html=True)
+
 st.write("Select Model Confidence")
 
-confidence = float(st.slider( , 25, 100, 40)) / 100    
+confidence = float(st.slider( , 25, 100, 40)) / 100
     
 # Load Pre-trained ML Model
 model_path = Path(settings.DETECTION_MODEL)
