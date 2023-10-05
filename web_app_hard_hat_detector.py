a87f41f240c61740100adf605ac1bc0ae076f4eb
adamzabek <adam.zabek@innobiotix.com>
2023-10-05 14:35:25 +0200

    update

--- web_app_hard_hat_detector.py
+++ web_app_hard_hat_detector.py
@@ -8,66 +8,38 @@ import av
 from turn import get_ice_servers
 import logging
 import queue
-
+import streamlit as st
+from aiortc.contrib.media import MediaPlayer
+from aiortc.contrib.media import MediaRecorder
+import cv2
 from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
 logger = logging.getLogger(__name__)
 
-def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
-    image = frame.to_ndarray(format="bgr24")
-
-    # Run inference
-    blob = cv2.dnn.blobFromImage(
-        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
-    )
-    net.setInput(blob)
-    output = net.forward()
-
-    h, w = image.shape[:2]
-
-    # Convert the output array into a structured form.
-    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
-    output = output[output[:, 2] >= score_threshold]
-    detections = [
-        Detection(
-            class_id=int(detection[1]),
-            label=CLASSES[int(detection[1])],
-            score=float(detection[2]),
-            box=(detection[3:7] * np.array([w, h, w, h])),
-        )
-        for detection in output
-    ]
-
-    # Render bounding boxes and captions
-    for detection in detections:
-        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
-        color = COLORS[detection.class_id]
-        xmin, ymin, xmax, ymax = detection.box.astype("int")
-
-        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
-        cv2.putText(
-            image,
-            caption,
-            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
-            cv2.FONT_HERSHEY_SIMPLEX,
-            0.5,
-            color,
-            2,
-        )
-
-    result_queue.put(detections)
-
-    return av.VideoFrame.from_ndarray(image, format="bgr24")
-
 
 webrtc_ctx = webrtc_streamer(
-    key="object-detection",
-    mode=WebRtcMode.SENDRECV,
-    rtc_configuration={
-        "iceServers": get_ice_servers(),
-        "iceTransportPolicy": "relay",
-    },
-    #video_frame_callback=video_frame_callback,
-    media_stream_constraints={"video": True, "audio": False},
-    async_processing=True,
+    key="example", 
+    video_processor_factory=None,
+    rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]},
+    media_stream_constraints={
+        "video": True,
+        "audio": False
+    }
 )
+
+if webrtc_ctx.video_receiver:
+    # Initialize the VideoCapture object for IP camera
+    cap = cv2.VideoCapture("your_ip_camera_url")
+
+    while True:
+        # Read frame from IP camera
+        ret, frame = cap.read()
+
+        # Display the frame in Streamlit
+        if ret:
+            webrtc_ctx.video_receiver.process_frame(frame)
+            st.image(frame, channels="BGR")
+
+    # Release the VideoCapture object and cleanup
+    cap.release()
+    cv2.destroyAllWindows()
