b0075ab14a5078ce16cb67b5681475b5a4e777a5
adamzabek <adam.zabek@innobiotix.com>
2023-10-05 12:52:51 +0200

    update

--- web_app_hard_hat_detector.py
+++ web_app_hard_hat_detector.py
@@ -5,58 +5,63 @@ from ultralytics import YOLO
 from pathlib import Path
 import settings
 
-# Load YOLO model
-model_path = Path(settings.DETECTION_MODEL)
+from streamlit_webrtc import WebRtcMode, webrtc_streamer
 
-# Set the title of the Streamlit app
-st.title("Real-Time YOLO Object Detection in Streamlit")
+def video_frame_callback(frame: av.VideoFrame) -> av.VideoFrame:
+    image = frame.to_ndarray(format="bgr24")
 
-# HTML code to access webcam and display video
-html_code = """
-<video id="webcam" width="100%" height="100%" autoplay></video>
-<button id="fullscreenButton">Toggle Fullscreen</button>
-<style>
-    body {
-        margin: 0;
-        padding: 0;
-        overflow: hidden;
-    }
-    #webcam {
-        object-fit: cover;
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
-</style>
-<script>
-    const video = document.getElementById('webcam');
-    const fullscreenButton = document.getElementById('fullscreenButton');
+    # Run inference
+    blob = cv2.dnn.blobFromImage(
+        cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5
+    )
+    net.setInput(blob)
+    output = net.forward()
 
-    // Function to toggle fullscreen mode
-    function toggleFullscreen() {
-        if (document.fullscreenElement) {
-            document.exitFullscreen();
-        } else {
-            video.requestFullscreen()
-                .catch(err => {
-                    console.error('Error attempting to enable fullscreen:', err.message);
-                });
-        }
-    }
+    h, w = image.shape[:2]
 
-    // Initialize webcam
-    async function setupWebcam() {
-        const stream = await navigator.mediaDevices.getUserMedia({ 'video': true });
-        video.srcObject = stream;
-    }
-    setupWebcam();
+    # Convert the output array into a structured form.
+    output = output.squeeze()  # (1, 1, N, 7) -> (N, 7)
+    output = output[output[:, 2] >= score_threshold]
+    detections = [
+        Detection(
+            class_id=int(detection[1]),
+            label=CLASSES[int(detection[1])],
+            score=float(detection[2]),
+            box=(detection[3:7] * np.array([w, h, w, h])),
+        )
+        for detection in output
+    ]
 
-    // Attach click event listener to the fullscreen button
-    fullscreenButton.addEventListener("click", toggleFullscreen);
-</script>
-"""
+    # Render bounding boxes and captions
+    for detection in detections:
+        caption = f"{detection.label}: {round(detection.score * 100, 2)}%"
+        color = COLORS[detection.class_id]
+        xmin, ymin, xmax, ymax = detection.box.astype("int")
 
-# Display the fullscreen webcam and object detection canvas using HTML and JavaScript
-st.components.v1.html(html_code)
+        cv2.rectangle(image, (xmin, ymin), (xmax, ymax), color, 2)
+        cv2.putText(
+            image,
+            caption,
+            (xmin, ymin - 15 if ymin - 15 > 15 else ymin + 15),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            color,
+            2,
+        )
+
+    result_queue.put(detections)
+
+    return av.VideoFrame.from_ndarray(image, format="bgr24")
+
+
+webrtc_ctx = webrtc_streamer(
+    key="object-detection",
+    mode=WebRtcMode.SENDRECV,
+    rtc_configuration={
+        "iceServers": get_ice_servers(),
+        "iceTransportPolicy": "relay",
+    },
+    video_frame_callback=video_frame_callback,
+    media_stream_constraints={"video": True, "audio": False},
+    async_processing=True,
+)
